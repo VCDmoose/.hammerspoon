bbdb39e72f80724dd4ccd764311e919f60766c62
Varuna C Dev <vcd@Varunas-MacBook-Pro.local>
2022-04-25 17:27:53 +0530

    Update shortcuts to use alt+ctrl for opening apps

--- init.lua
+++ init.lua
@@ -1,11 +1,30 @@
 -- Open apps
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "T", function()
+hs.hotkey.bind({"alt", "ctrl"}, "T", function()
     hs.application.launchOrFocus("iTerm")
   end)
 
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "I", function()
+
+hs.hotkey.bind({"alt", "ctrl"}, "F", function()
+    hs.application.launchOrFocus("Firefox")
+  end)
+
+
+hs.hotkey.bind({"alt", "ctrl"}, "C", function()
+    hs.application.launchOrFocus("Google Chrome")
+  end)
+
+hs.hotkey.bind({"alt", "ctrl"}, "I", function()
    hs.application.launchOrFocus("IntelliJ IDEA")
 end)
+
+hs.hotkey.bind({"alt", "ctrl"}, "S", function()
+   hs.application.launchOrFocus("Spotify")
+end)
+
+hs.hotkey.bind({"alt", "ctrl"}, "O", function()
+   hs.application.launchOrFocus("Obsidian")
+end)
+
 
 -- Move mouse cursor between screens
 hs.hotkey.bind({"alt", "ctrl"}, "Left", function()
